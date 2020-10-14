4feea785bc42d2813227f7af5de6ceca6c82433a
lin-mt <lin-mt@outlook.com>
2020-10-14 08:06:36 +0800

    枚举类型数据字典使用枚举类

--- src/services/system/Dictionary.ts
+++ src/services/system/Dictionary.ts
@@ -1,9 +1,10 @@
-export const Gender = {
-  MALE: '男',
-  FEMALE: '女',
-};
+// value 仅仅只是提供开发时理解，没有任何业务意义
+export enum Gender {
+  MALE = '男',
+  FEMALE = '女',
+}
 
-export const Weather = {
-  YES: '是',
-  NO: '否',
-};
+export enum Weather {
+  YES = '是',
+  NO = '否'
+}
