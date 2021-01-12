af29987d26d8c12e67e4d2f16af1b2c480c628dc
lin-mt <lin-mt@outlook.com>
2021-01-12 08:33:57 +0800

    修改权限判断方法

--- src/access.ts
+++ src/access.ts
@@ -1,7 +1,21 @@
 // src/access.ts
-export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
+export default function access(initialState: {
+  currentUser?: SystemEntities.QuietUser | undefined;
+}) {
   const { currentUser } = initialState || {};
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    SystemAdmin: currentUser && hasRole(currentUser.authorities, 'SystemAdmin'),
   };
+}
+
+function hasRole(roles: SystemEntities.QuietRole[], roleName: string) {
+  let hasRoleName = false;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const role of roles) {
+    if (role.roleName === roleName) {
+      hasRoleName = true;
+      break;
+    }
+  }
+  return hasRoleName;
 }
--- src/services/API.d.ts
+++ src/services/API.d.ts
@@ -1,19 +1,4 @@
 declare namespace API {
-  export type CurrentUser = {
-    avatar?: string;
-    name?: string;
-    title?: string;
-    group?: string;
-    signature?: string;
-    tags?: {
-      key: string;
-      label: string;
-    }[];
-    userid?: string;
-    access?: 'user' | 'guest' | 'admin';
-    unreadCount?: number;
-  };
-
   export type LoginStateType = {
     status?: 'ok' | 'error';
     type?: string;
