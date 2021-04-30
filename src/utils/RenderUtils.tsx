f3bade1b1f91153134327e8710683721d7de067c
lin-mt <lin-mt@outlook.com>
2021-04-30 10:21:01 +0800

    refactor the util code.

--- src/utils/RenderUtils.tsx
+++ src/utils/RenderUtils.tsx
@@ -1,5 +1,6 @@
 import type { CustomTagProps } from 'rc-select/lib/interface/generator';
 import { Tag } from 'antd';
+import React from 'react';
 
 export const tagColor = '#108EE9';
 
@@ -10,4 +11,22 @@ export function multipleSelectTagRender(tagProps: CustomTagProps) {
       {label}
     </Tag>
   );
+}
+
+export function buildFullCard(
+  cards: any[],
+  num: number,
+  addCard?: boolean,
+  addCardKey?: string,
+): any[] {
+  let fullCard: React.SetStateAction<any[]> = [];
+  if (addCard) {
+    fullCard.push({ key: addCardKey });
+  }
+  fullCard = fullCard.concat(cards);
+  const addEmptyCard = num - (fullCard.length % num);
+  for (let i = 0; i < addEmptyCard; i += 1) {
+    fullCard.push({ key: `empty${i}` });
+  }
+  return fullCard;
 }
