cba7760be640a932b335136867f241807f756d9a
lin-mt <lin-mt@outlook.com>
2021-05-03 11:14:13 +0800

    simple TemplateList.

--- src/models/scrum/TemplateList.ts
+++ src/models/scrum/TemplateList.ts
@@ -1,36 +1,7 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
 export default () => {
-  const [templateList, setTemplateList] = useState<Record<string, ScrumEntities.ScrumTemplate>>({});
+  const [templates, setTemplates] = useState<Record<string, ScrumEntities.ScrumTemplate>>({});
 
-  const addOrUpdateTemplate = useCallback(
-    (template: ScrumEntities.ScrumTemplate) => {
-      templateList[template.id] = template;
-    },
-    [templateList],
-  );
-
-  const addTemplates = useCallback(
-    (templates: ScrumEntities.ScrumTemplate[]) => {
-      templates.forEach((template) => {
-        templateList[template.id] = template;
-      });
-    },
-    [templateList],
-  );
-
-  const deleteTemplate = useCallback(
-    (id: string): void => {
-      const newTemplateList: Record<string, ScrumEntities.ScrumTemplate> = {};
-      Object.keys(templateList).forEach((key) => {
-        if (key !== id) {
-          newTemplateList[key] = templateList[key];
-        }
-      });
-      setTemplateList(newTemplateList);
-    },
-    [templateList],
-  );
-
-  return { templateList, addTemplates, addOrUpdateTemplate, deleteTemplate };
+  return { templates, setTemplates };
 };
