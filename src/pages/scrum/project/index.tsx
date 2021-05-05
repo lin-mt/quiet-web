3411e2bbc6056c8f59a780e862976db69578897e
lin-mt <lin-mt@outlook.com>
2021-05-05 12:08:59 +0800

    init demand type.

--- src/pages/scrum/project/index.tsx
+++ src/pages/scrum/project/index.tsx
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { allMyProjects } from '@/services/scrum/ScrumProject';
 import ProjectList from '@/pages/scrum/project/components/ProjectList';
 import type { ScrumProject } from '@/services/scrum/EntitiyType';
+import { useModel } from 'umi';
+import { DICTIONARY } from '@/constant/system/Modelnames';
+import { DictionaryType } from '@/types/Type';
 
 const Project: React.FC<any> = () => {
+  const { initDictionaries } = useModel(DICTIONARY);
+
   const [projectManaged, setProjectManaged] = useState<ScrumProject[]>([]);
   const [projectInvolved, setProjectInvolved] = useState<ScrumProject[]>([]);
 
@@ -15,8 +20,9 @@ const Project: React.FC<any> = () => {
   }
 
   useEffect(() => {
+    initDictionaries([DictionaryType.DemandType]);
     loadAllMyProject();
-  }, []);
+  }, [initDictionaries]);
 
   return (
     <>
