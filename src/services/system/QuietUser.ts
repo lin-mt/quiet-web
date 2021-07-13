aa68e0ab784be0ac56b8525fd8219732b2997f52
lin-mt <lin-mt@outlook.com>
2021-07-13 23:22:21 +0800

    refactor: reconstruction way of the QuietUser's request.

--- src/services/system/QuietUser.ts
+++ src/services/system/QuietUser.ts
@@ -1,9 +1,8 @@
 import { request } from 'umi';
-import type { Result } from '@/types/Result';
 import type { ReactText } from 'react';
 import type { NoticeIconData, QuietUser, QuietUserRole } from '@/services/system/EntityType';
 import type { RequestData } from '@ant-design/pro-table/lib/typing';
-import { GET } from '@/utils/HttpUtils';
+import { DELETE, GET, PAGE, POST, PUT } from '@/utils/HttpUtils';
 
 const baseUrl = '/api/system/user';
 
@@ -12,33 +11,19 @@ export function listUsersByName(keyword: string): Promise<QuietUser[]> {
 }
 
 export async function pageUser(params?: any): Promise<Partial<RequestData<QuietUser>>> {
-  return request<Result<Partial<RequestData<QuietUser>>>>(`${baseUrl}/page`, {
-    method: 'POST',
-    data: params,
-  }).then((resData) => {
-    return { ...resData.data, data: resData.data.results };
-  });
+  return PAGE<QuietUser>(`${baseUrl}/page`, params);
 }
 
 export async function registeredUser(save: QuietUser): Promise<QuietUser> {
-  return request<Result<QuietUser>>(`${baseUrl}/registered`, {
-    method: 'POST',
-    data: { save },
-  }).then((resp) => resp.data);
+  return POST<QuietUser>(`${baseUrl}`, save);
 }
 
-export async function deleteUser(deleteId: string) {
-  request(`${baseUrl}/delete`, {
-    method: 'POST',
-    data: { deleteId },
-  });
+export async function deleteUser(id: string) {
+  DELETE(`${baseUrl}/${id}`);
 }
 
 export async function updateUser(update: QuietUser): Promise<QuietUser> {
-  return request<Result<QuietUser>>(`${baseUrl}/update`, {
-    method: 'POST',
-    data: { update },
-  }).then((resp) => resp.data);
+  return PUT<QuietUser>(`${baseUrl}`, update);
 }
 
 export async function getNotices(options?: Record<string, any>) {
@@ -57,20 +42,14 @@ export async function queryCurrent(): Promise<QuietUser> {
   });
 }
 
-export async function removeRole(userId: string, roleId: string) {
-  return request(`${baseUrl}/removeRole`, {
-    data: { params: { userId, roleId } },
-    method: 'POST',
-  });
+export async function removeRole(id: string, roleId: string) {
+  return POST(`${baseUrl}/removeRole`, { id, roleId });
 }
 
 export async function addRoles(
-  saveBatch: { userId: string; roleId: ReactText }[],
+  userRoles: { userId: string; roleId: ReactText }[],
 ): Promise<QuietUserRole> {
-  return request<Result<QuietUserRole>>(`${baseUrl}/addRoles`, {
-    method: 'POST',
-    data: { saveBatch },
-  }).then((resp) => resp.data);
+  return POST<QuietUserRole>(`${baseUrl}/addRoles`, { userRoles });
 }
 
 export async function queryNotices(): Promise<any> {
