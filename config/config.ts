4bf64013d5a610f21ebbf6786b769db3ec746642
lin-mt <lin-mt@outlook.com>
2021-07-21 21:59:29 +0800

    feat: update config.

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,7 @@
 // https://umijs.org/config/
 import { defineConfig } from 'umi';
+import { join } from 'path';
+
 import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import routes from './routes';
@@ -49,4 +51,22 @@ export default defineConfig({
   },
   // Fast Refresh 热更新
   fastRefresh: {},
+  openAPI: [
+    {
+      requestLibPath: "import { request } from 'umi'",
+      // 或者使用在线的版本
+      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
+      schemaPath: join(__dirname, 'oneapi.json'),
+      mock: false,
+    },
+    {
+      requestLibPath: "import { request } from 'umi'",
+      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+      projectName: 'swagger',
+    },
+  ],
+  nodeModulesTransform: { type: 'none' },
+  mfsu: {},
+  webpack5: {},
+  exportStatic: {},
 });
