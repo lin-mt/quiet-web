3bffd740b0bd083f6689718bd32180b8c4911421
lin-mt <lin-mt@outlook.com>
2021-11-05 21:21:59 +0800

    UI: adjust style.

--- src/pages/doc/project/detail/components/ApiRun.tsx
+++ src/pages/doc/project/detail/components/ApiRun.tsx
@@ -29,7 +29,7 @@ interface ApiRunProps {
 export default function ApiRun(props: ApiRunProps) {
   const { apiDetail, projectInfo } = props;
   const [environments, setEnvironments] = useState<DocProjectEnvironment[]>([]);
-  const [selectEnv, setSelectEnv] = useState<string>('');
+  const [selectEnvIndex, setSelectEnvIndex] = useState<number | undefined>();
   const [formDataFiles, setFormDataFiles] = useState<Record<string, RcFile[]>>({});
   const [fileList, setFileList] = useState<RcFile[]>([]);
   const editorRef = useRef<any>(null);
@@ -42,14 +42,12 @@ export default function ApiRun(props: ApiRunProps) {
 
   useEffect(() => {
     if (environments.length > 0) {
-      if (environments[0].id) {
-        setSelectEnv(environments[0].id);
-      }
+      setSelectEnvIndex(0);
     }
   }, [environments]);
 
   return (
-    <div style={{ paddingLeft: 10, paddingRight: 10, paddingBottom: 20 }}>
+    <div style={{ paddingLeft: 30, paddingRight: 30, paddingBottom: 20 }}>
       <Input.Group>
         <Input
           value={apiDetail.api.method}
@@ -58,18 +56,16 @@ export default function ApiRun(props: ApiRunProps) {
         />
         {environments.length > 0 ? (
           <Select
-            value={selectEnv}
+            value={selectEnvIndex}
             style={{ width: '20%' }}
-            onChange={(envId) => setSelectEnv(envId)}
+            onChange={(envIndex) => setSelectEnvIndex(envIndex)}
           >
-            {environments.map((environment) => {
+            {environments.map((environment, index) => {
               if (environment.id) {
                 return (
-                  <Select.Option key={environment.id} value={environment.id}>{`${
-                    environment.name
-                  }：${environment.protocol === HttpProtocol.HTTP ? 'http://' : 'https://'}${
-                    environment.base_path
-                  }`}</Select.Option>
+                  <Select.Option key={environment.id} value={index}>{`${environment.name}：${
+                    environment.protocol === HttpProtocol.HTTP ? 'http://' : 'https://'
+                  }${environment.base_path}`}</Select.Option>
                 );
               }
               return;
@@ -91,12 +87,37 @@ export default function ApiRun(props: ApiRunProps) {
           发送
         </Button>
       </Input.Group>
-      <Row gutter={21} style={{ marginTop: 10 }}>
-        <Col span={12}>
-          {apiDetail.api_info?.headers && (
-            <div>
-              <ReqParamTitle style={{ marginTop: 0 }}>Header</ReqParamTitle>
-              {apiDetail.api_info.headers.map((header, index) => {
+      <div style={{ marginTop: 10 }}>
+        {(apiDetail.api_info?.headers ||
+          (selectEnvIndex && environments[selectEnvIndex].headers)) && (
+          <div>
+            <ReqParamTitle style={{ marginTop: 0 }}>Header</ReqParamTitle>
+            {apiDetail.api_info?.headers.map((header, index) => {
+              return (
+                <Row
+                  key={header.name}
+                  gutter={5}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    marginTop: index === 0 ? 0 : 5,
+                  }}
+                >
+                  <Col flex={'30%'}>
+                    <DisabledInput value={header.name} disabled={true} />
+                  </Col>
+                  <Col>=</Col>
+                  <Col>
+                    <Checkbox checked={header.required} disabled={true} />
+                  </Col>
+                  <Col flex={'auto'}>
+                    <Input value={header.value} />
+                  </Col>
+                </Row>
+              );
+            })}
+            {selectEnvIndex &&
+              environments[selectEnvIndex].headers?.map((header, index) => {
                 return (
                   <Row
                     key={header.name}
@@ -107,7 +128,7 @@ export default function ApiRun(props: ApiRunProps) {
                       marginTop: index === 0 ? 0 : 5,
                     }}
                   >
-                    <Col flex={'220px'}>
+                    <Col flex={'30%'}>
                       <DisabledInput value={header.name} disabled={true} />
                     </Col>
                     <Col>=</Col>
@@ -115,203 +136,204 @@ export default function ApiRun(props: ApiRunProps) {
                       <Checkbox checked={header.required} disabled={true} />
                     </Col>
                     <Col flex={'auto'}>
-                      <Input value={header.value} />
+                      <DisabledInput value={header.value} />
                     </Col>
                   </Row>
                 );
               })}
-            </div>
-          )}
-          {apiDetail.api_info?.path_param && (
-            <div>
-              <ReqParamTitle>PathParameter</ReqParamTitle>
-              {apiDetail.api_info.path_param.map((param, index) => {
-                return (
-                  <Row
-                    key={param.name}
-                    gutter={5}
-                    style={{
-                      display: 'flex',
-                      alignItems: 'baseline',
-                      marginTop: index === 0 ? 0 : 5,
-                    }}
-                  >
-                    <Col flex={'220px'}>
-                      <DisabledInput value={param.name} disabled={true} />
-                    </Col>
-                    <Col>=</Col>
-                    <Col flex={'auto'}>
-                      <Input value={param.example} />
-                    </Col>
-                  </Row>
-                );
-              })}
-            </div>
-          )}
-          {apiDetail.api_info?.req_query && (
-            <div>
-              <ReqParamTitle>Query</ReqParamTitle>
-              {apiDetail.api_info.req_query.map((query, index) => {
-                return (
-                  <Row
-                    key={query.name}
-                    gutter={5}
-                    style={{
-                      display: 'flex',
-                      alignItems: 'baseline',
-                      marginTop: index === 0 ? 0 : 5,
-                    }}
-                  >
-                    <Col flex={'220px'}>
-                      <DisabledInput value={query.name} disabled={true} />
-                    </Col>
-                    <Col>=</Col>
-                    <Col>
-                      <Checkbox checked={query.required} disabled={true} />
-                    </Col>
-                    <Col flex={'auto'}>
-                      <Input value={query.example} />
-                    </Col>
-                  </Row>
-                );
-              })}
-            </div>
-          )}
-          {(apiDetail.api_info?.req_form ||
-            apiDetail.api_info?.req_raw ||
-            apiDetail.api_info?.req_file ||
-            apiDetail.api_info?.req_json_body) && (
-            <div>
-              <ReqParamTitle>Body</ReqParamTitle>
-              {apiDetail.api_info.req_form.map((param, index) => {
-                return (
-                  <Row
-                    key={param.name}
-                    gutter={5}
-                    style={{
-                      display: 'flex',
-                      alignItems: 'baseline',
-                      marginTop: index === 0 ? 0 : 8,
-                    }}
-                  >
-                    <Col flex={'220px'}>
-                      <DisabledInput value={param.name} disabled={true} />
-                    </Col>
-                    <Col>=</Col>
-                    <Col>
-                      <Checkbox checked={param.required} disabled={true} />
-                    </Col>
-                    {
-                      // fixme 文件名称长度过长会破坏排版
-                    }
-                    <Col flex={'auto'}>
-                      {param.type === FormParamType.TEXT && <Input value={param.example} />}
-                      {param.type === FormParamType.FILE && (
-                        <Upload
-                          fileList={formDataFiles[param.name]}
-                          beforeUpload={(file: RcFile) => {
-                            let datumList: RcFile[];
-                            if (formDataFiles[param.name]) {
-                              let duplicate = false;
-                              formDataFiles[param.name].forEach((datum) => {
-                                if (datum.name === file.name) {
-                                  duplicate = true;
-                                }
-                              });
-                              if (!duplicate) {
-                                datumList = [...formDataFiles[param.name], file];
-                              } else {
-                                return false;
-                              }
-                            } else {
-                              datumList = [file];
-                            }
-                            setFormDataFiles({ ...formDataFiles, [param.name]: datumList });
-                            return false;
-                          }}
-                          onRemove={(file: UploadFile) => {
-                            const files = formDataFiles[param.name];
-                            const newFileList = files.filter((datum) => datum.name !== file.name);
-                            setFormDataFiles({ ...formDataFiles, [param.name]: newFileList });
-                          }}
-                        >
-                          <Button size={'small'} icon={<UploadOutlined />}>
-                            选择文件
-                          </Button>
-                        </Upload>
-                      )}
-                    </Col>
-                  </Row>
-                );
-              })}
-
-              {apiDetail.api_info.req_file && (
+          </div>
+        )}
+        {apiDetail.api_info?.path_param && (
+          <div>
+            <ReqParamTitle>PathParameter</ReqParamTitle>
+            {apiDetail.api_info.path_param.map((param, index) => {
+              return (
                 <Row
+                  key={param.name}
                   gutter={5}
                   style={{
                     display: 'flex',
                     alignItems: 'baseline',
+                    marginTop: index === 0 ? 0 : 5,
                   }}
                 >
-                  <Col flex={'220px'}>
-                    <DisabledInput value={apiDetail.api_info.req_file} disabled={true} />
+                  <Col flex={'30%'}>
+                    <DisabledInput value={param.name} disabled={true} />
                   </Col>
                   <Col>=</Col>
                   <Col flex={'auto'}>
-                    <Upload
-                      fileList={fileList}
-                      beforeUpload={(file: RcFile) => {
-                        const newFileList = fileList.filter((datum) => datum.name !== file.name);
-                        newFileList.push(file);
-                        return false;
-                      }}
-                      onRemove={(file) => {
-                        setFileList(fileList.filter((datum) => datum.name !== file.name));
-                      }}
-                    >
-                      <Button size={'small'} icon={<UploadOutlined />}>
-                        选择文件
-                      </Button>
-                    </Upload>
+                    <Input value={param.example} />
                   </Col>
                 </Row>
-              )}
-              {(apiDetail.api_info?.req_raw || apiDetail.api_info?.req_json_body) && (
-                <div>
-                  <Button
-                    type={'primary'}
-                    onClick={() => {
-                      if (editorRef.current !== null) {
-                        editorRef.current.getAction('editor.action.formatDocument').run();
-                      }
+              );
+            })}
+          </div>
+        )}
+        {apiDetail.api_info?.req_query && (
+          <div>
+            <ReqParamTitle>Query</ReqParamTitle>
+            {apiDetail.api_info.req_query.map((query, index) => {
+              return (
+                <Row
+                  key={query.name}
+                  gutter={5}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    marginTop: index === 0 ? 0 : 5,
+                  }}
+                >
+                  <Col flex={'30%'}>
+                    <DisabledInput value={query.name} disabled={true} />
+                  </Col>
+                  <Col>=</Col>
+                  <Col>
+                    <Checkbox checked={query.required} disabled={true} />
+                  </Col>
+                  <Col flex={'auto'}>
+                    <Input value={query.example} />
+                  </Col>
+                </Row>
+              );
+            })}
+          </div>
+        )}
+        {(apiDetail.api_info?.req_form ||
+          apiDetail.api_info?.req_raw ||
+          apiDetail.api_info?.req_file ||
+          apiDetail.api_info?.req_json_body) && (
+          <div>
+            <ReqParamTitle>Body</ReqParamTitle>
+            {apiDetail.api_info.req_form.map((param, index) => {
+              return (
+                <Row
+                  key={param.name}
+                  gutter={5}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    marginTop: index === 0 ? 0 : 8,
+                  }}
+                >
+                  <Col flex={'30%'}>
+                    <DisabledInput value={param.name} disabled={true} />
+                  </Col>
+                  <Col>=</Col>
+                  <Col>
+                    <Checkbox checked={param.required} disabled={true} />
+                  </Col>
+                  {
+                    // fixme 文件名称长度过长会破坏排版
+                  }
+                  <Col flex={'auto'}>
+                    {param.type === FormParamType.TEXT && <Input value={param.example} />}
+                    {param.type === FormParamType.FILE && (
+                      <Upload
+                        fileList={formDataFiles[param.name]}
+                        beforeUpload={(file: RcFile) => {
+                          let datumList: RcFile[];
+                          if (formDataFiles[param.name]) {
+                            let duplicate = false;
+                            formDataFiles[param.name].forEach((datum) => {
+                              if (datum.name === file.name) {
+                                duplicate = true;
+                              }
+                            });
+                            if (!duplicate) {
+                              datumList = [...formDataFiles[param.name], file];
+                            } else {
+                              return false;
+                            }
+                          } else {
+                            datumList = [file];
+                          }
+                          setFormDataFiles({ ...formDataFiles, [param.name]: datumList });
+                          return false;
+                        }}
+                        onRemove={(file: UploadFile) => {
+                          const files = formDataFiles[param.name];
+                          const newFileList = files.filter((datum) => datum.name !== file.name);
+                          setFormDataFiles({ ...formDataFiles, [param.name]: newFileList });
+                        }}
+                      >
+                        <Button size={'small'} icon={<UploadOutlined />}>
+                          选择文件
+                        </Button>
+                      </Upload>
+                    )}
+                  </Col>
+                </Row>
+              );
+            })}
+
+            {apiDetail.api_info.req_file && (
+              <Row
+                gutter={5}
+                style={{
+                  display: 'flex',
+                  alignItems: 'baseline',
+                }}
+              >
+                <Col flex={'30%'}>
+                  <DisabledInput value={apiDetail.api_info.req_file} disabled={true} />
+                </Col>
+                <Col>=</Col>
+                <Col flex={'auto'}>
+                  <Upload
+                    fileList={fileList}
+                    beforeUpload={(file: RcFile) => {
+                      const newFileList = fileList.filter((datum) => datum.name !== file.name);
+                      newFileList.push(file);
+                      return false;
+                    }}
+                    onRemove={(file) => {
+                      setFileList(fileList.filter((datum) => datum.name !== file.name));
                     }}
                   >
-                    格式化
-                  </Button>
-                  <div
-                    style={{
-                      borderWidth: 1,
-                      borderColor: '#d9d9d9',
-                      borderStyle: 'solid',
-                      marginTop: 10,
-                    }}
-                  >
-                    <QuietEditor
-                      defaultValue={
-                        apiDetail.api_info?.req_raw
-                          ? apiDetail.api_info?.req_raw
-                          : JSON.stringify(apiDetail.api_info?.req_json_body)
-                      }
-                      language={apiDetail.api_info?.req_json_body ? 'json' : undefined}
-                      onMount={(editor) => (editorRef.current = editor)}
-                      height={600}
-                    />
-                  </div>
+                    <Button size={'small'} icon={<UploadOutlined />}>
+                      选择文件
+                    </Button>
+                  </Upload>
+                </Col>
+              </Row>
+            )}
+            {(apiDetail.api_info?.req_raw || apiDetail.api_info?.req_json_body) && (
+              <div>
+                <Button
+                  type={'primary'}
+                  onClick={() => {
+                    if (editorRef.current !== null) {
+                      editorRef.current.getAction('editor.action.formatDocument').run();
+                    }
+                  }}
+                >
+                  格式化
+                </Button>
+                <div
+                  style={{
+                    borderWidth: 1,
+                    borderColor: '#d9d9d9',
+                    borderStyle: 'solid',
+                    marginTop: 10,
+                  }}
+                >
+                  <QuietEditor
+                    defaultValue={
+                      apiDetail.api_info?.req_raw
+                        ? apiDetail.api_info?.req_raw
+                        : JSON.stringify(apiDetail.api_info?.req_json_body)
+                    }
+                    language={apiDetail.api_info?.req_json_body ? 'json' : undefined}
+                    onMount={(editor) => (editorRef.current = editor)}
+                    height={369}
+                  />
                 </div>
-              )}
-            </div>
-          )}
-        </Col>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <Row>
         <Col span={12}>{props.projectInfo.id}</Col>
       </Row>
     </div>
