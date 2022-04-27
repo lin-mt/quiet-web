e5180d8b448c3b4b5e2551e5eeb3c116e3bbdf93
lin-mt <lin-mt@outlook.com>
2022-04-27 10:13:15 +0800

    feat: update markdown editor.

--- src/pages/components/Markdown/MarkdownEditor/index.tsx
+++ src/pages/components/Markdown/MarkdownEditor/index.tsx
@@ -5,7 +5,7 @@ import 'bytemd/dist/index.css'; // bytemd 基础样式
 import 'github-markdown-css/github-markdown.css';
 import 'highlight.js/styles/xcode.css';
 import './index.css';
-import zhHans from 'bytemd/lib/locales/zh_Hans.json';
+import zhHans from 'bytemd/locales/zh_Hans.json';
 import { plugins } from '@/pages/components/Markdown/config';
 
 interface MarkdownEditorProp {
--- src/pages/components/Markdown/config.ts
+++ src/pages/components/Markdown/config.ts
@@ -1,5 +1,4 @@
 import breaks from '@bytemd/plugin-breaks';
-import footnotes from '@bytemd/plugin-footnotes';
 import frontmatter from '@bytemd/plugin-frontmatter'; // 头部元信息
 import gemoji from '@bytemd/plugin-gemoji'; // emoji😊 代码
 import gfm from '@bytemd/plugin-gfm'; // 超链接、删除线、表格、任务列表
@@ -7,13 +6,12 @@ import highlight from '@bytemd/plugin-highlight';
 import math from '@bytemd/plugin-math'; // 数学公式
 import mediumZoom from '@bytemd/plugin-medium-zoom';
 import mermaid from '@bytemd/plugin-mermaid'; // 图表 / 流程图
-import mermaid_zhHans from '@bytemd/plugin-mermaid/lib/locales/zh_Hans.json';
-import math_zhHans from '@bytemd/plugin-math/lib/locales/zh_Hans.json';
-import gfm_zhHans from '@bytemd/plugin-gfm/lib/locales/zh_Hans.json';
+import mermaid_zhHans from '@bytemd/plugin-mermaid/locales/zh_Hans.json';
+import math_zhHans from '@bytemd/plugin-math/locales/zh_Hans.json';
+import gfm_zhHans from '@bytemd/plugin-gfm/locales/zh_Hans.json';
 
 export const plugins = [
   breaks(),
-  footnotes(),
   frontmatter(),
   gemoji(),
   gfm({ locale: gfm_zhHans }),
