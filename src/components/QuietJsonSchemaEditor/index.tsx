b95a09506462e9a3557f4ebea65b29acc92d5894
lin-mt <lin-mt@outlook.com>
2022-09-21 20:42:42 +0800

    feat: add monaco-editor.

--- src/components/QuietJsonSchemaEditor/index.tsx
+++ src/components/QuietJsonSchemaEditor/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import JsonSchemaEditor from '@quiet-front-end/json-schema-editor-arco';
 import '@quiet-front-end/json-schema-editor-arco/dist/css/index.css';
+import * as monaco from 'monaco-editor';
+import { loader } from '@monaco-editor/react';
+
+loader.config({ monaco });
 
 export type QuietJsonSchemaEditorProps = {
   value?;
