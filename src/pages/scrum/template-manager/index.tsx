487d55b6d834d20c515626ee9bccb658c98d6246
lin-mt <lin-mt@outlook.com>
2022-10-02 09:17:44 +0800

    feat: template manager.

--- src/pages/scrum/template-manager/index.tsx
+++ src/pages/scrum/template-manager/index.tsx
@@ -1,7 +1,130 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Grid, Notification } from '@arco-design/web-react';
+import SearchForm from '@/pages/scrum/template-manager/search-form';
+import styles from '@/pages/scrum/template-manager/style/index.module.less';
+import { ScrumTemplate } from '@/service/scrum/type';
+import TemplateForm, {
+  TemplateFormProps,
+} from '@/components/scrum/TemplateForm';
+import {
+  deleteTemplate,
+  getTemplateDetail,
+  listTemplate,
+  saveTemplate,
+  updateTemplate,
+} from '@/service/scrum/template';
+import CardAddTemplate from '@/pages/scrum/template-manager/card-add-template';
+import CardTemplate from '@/pages/scrum/template-manager/card-template';
+import { batchSavePriorities } from '@/service/scrum/priority';
+import { batchSaveTaskStep } from '@/service/scrum/task-step';
+
+const { Row, Col } = Grid;
 
 function TemplateManager() {
-  return <>模板管理</>;
+  const [templates, setTemplates] = useState<ScrumTemplate[]>([]);
+  const [params, setParams] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [templateFormProps, setTemplateFormProps] = useState<TemplateFormProps>(
+    {}
+  );
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(params)]);
+
+  function fetchData() {
+    setLoading(true);
+    listTemplate({ ...params, limit: 0 })
+      .then((resp) => setTemplates(resp))
+      .finally(() => setLoading(false));
+  }
+
+  function handleCreateTemplate() {
+    setTemplateFormProps({
+      visible: true,
+      title: '创建模板',
+      onOk: (values) => {
+        return saveTemplate(values).then(async (resp) => {
+          if (values.priorities) {
+            await batchSavePriorities(resp.id, values.priorities);
+          }
+          fetchData();
+          setTemplateFormProps({ visible: false });
+        });
+      },
+      onCancel: () => setTemplateFormProps({ visible: false }),
+    });
+  }
+
+  function handleUpdateTemplate(id) {
+    getTemplateDetail(id).then((resp) => {
+      setTemplateFormProps({
+        formValues: resp,
+        visible: true,
+        onOk: (values) => {
+          return updateTemplate(values).then(async (resp) => {
+            if (values.priorities) {
+              values.priorities.forEach((v) => (v.template_id = resp.id));
+              await batchSavePriorities(resp.id, values.priorities).catch(
+                () => {
+                  Notification.error({
+                    content: '优先级信息保存失败，请重试或者联系管理员',
+                  });
+                }
+              );
+            }
+            if (values.task_steps) {
+              values.task_steps.forEach((v) => (v.template_id = resp.id));
+              await batchSaveTaskStep(resp.id, values.task_steps).catch(() => {
+                Notification.error({
+                  content: '任务步骤信息保存失败，请重试或者联系管理员',
+                });
+              });
+            }
+            fetchData();
+            setTemplateFormProps({ visible: false });
+          });
+        },
+      });
+    });
+  }
+
+  return (
+    <div className={styles.container}>
+      <Card>
+        <SearchForm onSearch={(value) => setParams(value)} />
+        <Row gutter={24} className={styles['card-content']}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={6} xxl={6}>
+            <CardAddTemplate
+              description={'创建模板'}
+              onClick={handleCreateTemplate}
+            />
+          </Col>
+          {templates?.map((team) => {
+            return (
+              <Col key={team.id} xs={24} sm={12} md={8} lg={6} xl={6} xxl={6}>
+                <CardTemplate
+                  loading={loading}
+                  template={team}
+                  onEdit={(value) => handleUpdateTemplate(value.id)}
+                  onDelete={(id) => {
+                    deleteTemplate(id).then(() => {
+                      fetchData();
+                    });
+                  }}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Card>
+      <TemplateForm
+        onCancel={() => setTemplateFormProps({ visible: false })}
+        {...templateFormProps}
+      />
+    </div>
+  );
 }
 
 export default TemplateManager;
---
+++ src/service/scrum/type.ts
@@ -0,0 +1,88 @@
+import {
+  BaseEntity,
+  ParentEntity,
+  QuietTeam,
+  SortableEntity,
+} from '../system/type';
+
+export interface ScrumProject extends BaseEntity {
+  name: string;
+  manager: string;
+  template_id: string;
+  description?: string;
+  demand_prefix?: string;
+  task_prefix?: string;
+  template_name?: string;
+  build_tool?: string;
+  manager_name?: string;
+  teams?: QuietTeam[];
+}
+
+export interface ScrumTemplate extends BaseEntity {
+  name: string;
+  enabled: boolean;
+  remark?: string;
+  task_steps?: ScrumTaskStep[];
+  priorities?: ScrumPriority[];
+}
+
+export interface ScrumTaskStep extends SortableEntity {
+  name: string;
+  template_id: string;
+  remark?: string;
+}
+
+export interface ScrumPriority extends SortableEntity {
+  name: string;
+  color_hex: string;
+  template_id: string;
+  remark?: string;
+}
+
+export interface ScrumDemand extends SortableEntity, ParentEntity<ScrumDemand> {
+  title: string;
+  type: string;
+  project_id: string;
+  iteration_id?: string;
+  priority_id: string;
+  optimize_demand_id?: string;
+  start_time?: string;
+  end_time?: string;
+  remark?: string;
+}
+
+export interface ScrumTask extends SortableEntity {
+  title: string;
+  type: string;
+  demand_id: string;
+  task_step_id: string;
+  executor_id: string;
+  participant?: string[];
+  pre_task_ids?: string[];
+  start_time?: string;
+  end_time?: string;
+  remark: string;
+}
+
+export interface ScrumIteration extends SortableEntity {
+  name: string;
+  version_id: string;
+  plan_start_date: string;
+  plan_end_date: string;
+  start_time?: string;
+  end_time?: string;
+  remark?: string;
+}
+
+export interface ScrumVersion
+  extends ParentEntity<ScrumVersion>,
+    SortableEntity {
+  name: string;
+  project_id: string;
+  plan_start_date: string;
+  plan_end_date: string;
+  start_time?: string;
+  end_time?: string;
+  remark?: string;
+  iterations?: ScrumIteration[];
+}
